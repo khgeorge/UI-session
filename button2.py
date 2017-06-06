0ea3c6f1310964148540f6fbe6aa7ab554e0bb00
khgeorge <khillary.george@gmail.com>
2017-06-06 13:40:36 +0530

    just
    this is a tkinter button code

---
+++ button2.py
@@ -0,0 +1,14 @@
+
+import Tkinter
+import tkMessageBox
+
+top = Tkinter.Tk()
+
+def helloCallBack():
+   tkMessageBox.showinfo( "Hello Python", "Hello World")
+
+B = Tkinter.Button(top, text ="Book", command = helloCallBack)
+
+B.pack()
+top.mainloop()
+
